57cc873427b362fee3d1ef046a5f9192dbe5c58f
Shrillx <dubey.vandit@gmail.com>
2023-12-17 18:56:39 +0530

    v

--- middlewares/accessControlMiddleware.js
+++ middlewares/accessControlMiddleware.js
@@ -1,6 +1,7 @@
 const accessControlMiddleware = (req, res, next) => {
   const userRole = req.user ? req.user.role : null;
   console.log("req.user   " + req.user);
+  console.log(req)
   if (userRole === "admin") {
     next();
   } else {
