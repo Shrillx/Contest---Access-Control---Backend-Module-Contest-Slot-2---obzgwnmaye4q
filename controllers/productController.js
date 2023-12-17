597677d1a31359a0d9b53f43711c0762a4bd4458
Shrillx <dubey.vandit@gmail.com>
2023-12-17 18:07:17 +0530

    productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,6 +4,7 @@ const Product = require('../models/productModel');
 exports.getAllProducts = async (req, res) => {
   try {
     // TODO: Fetch all products from the database
+    const products = await Product.find();
     res.json({ message: 'Get all products', products });
   } catch (error) {
     console.error(error);
@@ -17,6 +18,16 @@ exports.createProduct = async (req, res) => {
     // TODO 1: Create a new Product and check if the role is admin if admin then, implement TODO 2
     // TODO 2: Create a new product, save it to the database, and send a success response
     // res.status(201).json({ message: 'Create a new product', product: newProduct });
+
+    const userRole = req.user.role; 
+    
+    if(userRole !== 'admin'){
+      return res.status(403).json({message: "Unauthorized"})
+    }
+    
+    const {name, price }= req.body; 
+    const newProduct = new Product({name,price});
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
